e1bea602fbb3bfaa8d449bd940de834512449ddf
reidchoi-wave <reid.choi@wavemediahk.com>
2023-05-22 11:46:11 +0800

    v1

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# pdf2text
+# How to run
+python main.py
+
+# Request
+pip install pdf2image
+pip install PIL
+pip install pytesseract
+
+# How to add more lang for OCR
+https://github.com/tesseract-ocr/tessdata/tree/main
---
+++ main.py
@@ -0,0 +1,49 @@
+# import module
+from pdf2image import convert_from_path
+from PIL import Image
+import pytesseract
+import os
+
+thePdfFile = './docs/急救手册-jxuspt.pdf'
+# Store Pdf with convert_from_path function
+images = convert_from_path({thePdfFile})
+text = ''
+
+numOfPdf = range(len(images))
+withoutArr = []
+
+
+# for i in range(len(images)):
+for i, image in enumerate(images):
+    tmpText = ''
+    imgLink = './tmp/page-' + str(i) + '.jpg'
+
+    print(f"looking paged: {i}/{numOfPdf} | {round(i / numOfPdf * 100, 2)} %")
+    
+    # without case
+    if i in withoutArr:
+        print(f'paged {i} without')
+    else:
+        print(f'paged {i} running')
+        # Save pages as images in the pdf
+        images[i].save(imgLink, 'JPEG')
+
+        # OCR the image
+        _img = Image.open(imgLink)
+
+        tmpText = pytesseract.image_to_string(_img, lang='chi_sim+eng')
+        tmpText = tmpText.replace("圖", "")
+        tmpText = tmpText.replace(" ", "")
+
+        text += tmpText
+
+        print(tmpText)
+
+        # remove the image file
+        os.remove(imgLink)
+
+
+# print(text)
+with open('./data-out.txt', 'w') as file:
+    file.write(text)
+    
